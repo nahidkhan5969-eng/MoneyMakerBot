59547a8262b381b89c6b278e183f4f0ec64a3fa7
nahidkhan5969-eng <nahidkhan5969@gmail.com>
2026-06-29 16:30:52 +0600

    Update ├── bot.py

--- ├── bot.py
+++ ├── bot.py
@@ -1,15 +1,16 @@
 
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "8308301984:AAFFioBRotihpg3_UkPBJ8Z5PowpJmmjO9o"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🎉 Welcome to Money Maker Bot!")
+    await update.message.reply_text("✅ Money Maker Bot is Running!")
 
 app = Application.builder().token(BOT_TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
 print("Bot Started...")
+
 app.run_polling()
